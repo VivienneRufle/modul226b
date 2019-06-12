8cc7a228f9efdf7d7b1f861d4bb9467a9965d2ae
AlexStammherr <43452647+AlexStammherr@users.noreply.github.com>
2019-06-12 15:14:52 +0200

    Java

--- TripleAction/src/viewController/RiskViewController.java
+++ TripleAction/src/viewController/RiskViewController.java
@@ -4,7 +4,6 @@ import heredity.GameCategoryController;
 
 public class RiskViewController implements GameCategoryController {
 
-	@Override
 	public int show(String playerName) {
 		// TODO Auto-generated method stub
 		return 0;
